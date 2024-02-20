c15b10fa1fb26b876f28af510558887463ced6b4
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-02-20 18:02:29 +0100

    Update copyright year

--- aiocsv/__init__.py
+++ aiocsv/__init__.py
@@ -7,7 +7,7 @@ __author__ = "Mikołaj Kuranowski"
 __email__ = "".join(chr(i) for i in [109, 107, 117, 114, 97, 110, 111, 119, 115, 107, 105,
                                      64, 103, 109, 97, 105, 108, 46, 99, 111, 109])
 
-__copyright__ = "© Copyright 2020-2023 Mikołaj Kuranowski"
+__copyright__ = "© Copyright 2020-2024 Mikołaj Kuranowski"
 __license__ = "MIT"
 
 from .readers import AsyncReader, AsyncDictReader
