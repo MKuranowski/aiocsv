0f73ceeb6bfe2ad9e8dbd4202e4abc7a054600ec
Mikołaj Kuranowski <mkuranowski@gmail.com>
2026-05-22 19:43:28 +0200

    v1.4.1 release

--- aiocsv/__init__.py
+++ aiocsv/__init__.py
@@ -1,15 +1,15 @@
-# © Copyright 2020-2024 Mikołaj Kuranowski
+# © Copyright 2020-2026 Mikołaj Kuranowski
 # SPDX-License-Identifier: MIT
 
 __title__ = "aiocsv"
 __description__ = "Asynchronous CSV reading/writing"
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 __url__ = "https://github.com/MKuranowski/aiocsv"
 __author__ = "Mikołaj Kuranowski"
 __email__ = "mkuranowski+pypackages@gmail.com"
 
-__copyright__ = "© Copyright 2020-2024 Mikołaj Kuranowski"
+__copyright__ = "© Copyright 2020-2026 Mikołaj Kuranowski"
 __license__ = "MIT"
 
 from .readers import AsyncDictReader, AsyncReader
